d8b15a182d6864aadad2a8d0b0e00468b9282700
divitas <diph8836@colorado.edu>
2025-03-01 12:01:35 -0700

    backend initial setup

---
+++ study-assistant-backend/src/test/java/com/hackcu/study_assistant/StudyAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackcu.study_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
