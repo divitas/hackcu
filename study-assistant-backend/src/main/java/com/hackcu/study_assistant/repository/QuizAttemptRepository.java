5f9aec99783a88d268d87ec6e4787142d6dcfe7f
divitas <diph8836@colorado.edu>
2025-03-01 18:07:32 -0700

    backend changes for student quiz and result

--- study-assistant-backend/src/main/java/com/hackcu/study_assistant/repository/QuizAttemptRepository.java
+++ study-assistant-backend/src/main/java/com/hackcu/study_assistant/repository/QuizAttemptRepository.java
@@ -1,4 +1,10 @@
 package com.hackcu.study_assistant.repository;
 
-public class QuizAttemptRepository {
+import com.hackcu.study_assistant.model.QuizAttempt;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface QuizAttemptRepository extends MongoRepository<QuizAttempt, String> {
+    @Query("{'studentId': ?0, 'quizId': ?1}")
+    QuizAttempt findByStudentAndQuizId(String studentId, String quizId);
 }
--- study-assistant-backend/src/main/java/com/hackcu/study_assistant/service/QuizService.java
+++ study-assistant-backend/src/main/java/com/hackcu/study_assistant/service/QuizService.java
@@ -1,4 +1,42 @@
 package com.hackcu.study_assistant.service;
 
+import com.hackcu.study_assistant.model.Question;
+import com.hackcu.study_assistant.model.Quiz;
+import com.hackcu.study_assistant.model.QuizAttempt;
+import com.hackcu.study_assistant.repository.QuizAttemptRepository;
+import com.hackcu.study_assistant.repository.QuizRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class QuizService {
+
+    @Autowired
+    QuizRepository quizRepository;
+
+    @Autowired
+    QuizAttemptRepository quizAttemptRepository;
+
+    public void createNewQuiz(List<Question> questions, String summary, String name) {
+        Quiz quiz = new Quiz(name, summary, questions);
+        quizRepository.save(quiz);
+    }
+
+    public void saveQuizAttempt(QuizAttempt quizAttempt) {
+        quizAttemptRepository.save(quizAttempt);
+    }
+
+    public List<Quiz> getQuizByName(String name) {
+        return quizRepository.findAllByName(name);
+    }
+
+    public QuizAttempt getQuizAttemptByIds(String studentId, String quizId) {
+        return quizAttemptRepository.findByStudentAndQuizId(studentId, quizId);
+    }
+
+    public List<Quiz> getAllQuizzes() {
+        return quizRepository.findAll();
+    }
 }
