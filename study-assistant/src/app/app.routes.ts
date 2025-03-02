14832b2e777c8f9416d84e2c5442740dbd4699a3
divitas <diph8836@colorado.edu>
2025-03-01 20:45:03 -0700

    merge changes for result component

--- study-assistant/src/app/app.routes.ts
+++ study-assistant/src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { QuizComponent } from './quiz/quiz.component';
 import { FlashcardComponent } from './flashcard/flashcard.component';
-import { ResultComponent } from './result/result.component';
 import { QuizListComponent } from './quiz-list/quiz-list.component';
 
 
 export const routes: Routes = [
     { path: 'quiz/:quizId', component: QuizComponent },
     {path: 'flashcard', component: FlashcardComponent },
-    { path: 'results', component: ResultComponent },
     {path: 'quizzes', component:QuizListComponent}
 ];
