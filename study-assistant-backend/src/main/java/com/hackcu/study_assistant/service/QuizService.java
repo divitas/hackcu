c449cfdf7fcecc49092c9c0747e9edae7e01bafe
divitas <diph8836@colorado.edu>
2025-03-01 20:15:27 -0700

    quiz list backend

--- study-assistant-backend/src/main/java/com/hackcu/study_assistant/service/QuizService.java
+++ study-assistant-backend/src/main/java/com/hackcu/study_assistant/service/QuizService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -38,5 +39,10 @@ public class QuizService {
 
     public List<Quiz> getAllQuizzes() {
         return quizRepository.findAll();
+    }
+
+    public Quiz getQuizById(String id) {
+        Optional<Quiz> quizResponse = quizRepository.findById(id);
+        return quizResponse.orElse(null);
     }
 }
